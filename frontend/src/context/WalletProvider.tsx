475c36df39528c55a5fc2e02440e3f4644877767
portley-0 <oisinportley@protonmail.com>
2025-03-29 10:59:54 +0000

    RainbowKit wallet fix

--- frontend/src/context/WalletProvider.tsx
+++ frontend/src/context/WalletProvider.tsx
@@ -1,31 +1,59 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import {
-  getDefaultConfig,
   RainbowKitProvider,
   darkTheme,
+  connectorsForWallets,
 } from "@rainbow-me/rainbowkit";
-import { WagmiProvider } from "wagmi";
-import { avalancheFuji } from "wagmi/chains";
 import {
-  QueryClientProvider,
-  QueryClient,
-} from "@tanstack/react-query";
+  rainbowWallet,
+  walletConnectWallet,
+  injectedWallet,
+  metaMaskWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { createConfig, http, WagmiProvider } from "wagmi";
+import { createClient as createViemClient } from "viem";
+import { avalancheFuji } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactNode } from "react";
 
-const queryClient = new QueryClient();
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended",
+      wallets: [
+        metaMaskWallet({ chains: [avalancheFuji] }),
+        injectedWallet({ chains: [avalancheFuji] }),
+        rainbowWallet({ chains: [avalancheFuji] }),
+        walletConnectWallet({ chains: [avalancheFuji] }),
+      ],
+    },
+  ],
+  {
+    appName: "LiveDuel Demo 2",
+    projectId: "e87d4bfd159146fc8d6fb15484d62a2c",
+  }
+);
 
-const config = getDefaultConfig({
-  appName: "LiveDuel Demo 2",
-  projectId: "e87d4bfd159146fc8d6fb15484d62a2c", 
+const config = createConfig({
   chains: [avalancheFuji],
+  connectors,
+  client({ chain }) {
+    return createViemClient({
+      chain,
+      transport: http(),
+    });
+  },
 });
 
-import { ReactNode } from "react";
+const queryClient = new QueryClient();
 
 export default function WalletProvider({ children }: { children: ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider theme={darkTheme()}>{children}</RainbowKitProvider>
+        <RainbowKitProvider chains={[avalancheFuji]} theme={darkTheme()}>
+          {children}
+        </RainbowKitProvider>
       </QueryClientProvider>
     </WagmiProvider>
   );
