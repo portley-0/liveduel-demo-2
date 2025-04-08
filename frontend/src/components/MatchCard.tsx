cabc19e9b04879407322948ec3ed4a3c346a9bc2
portley-0 <oisinportley@protonmail.com>
2025-04-08 01:47:34 +0100

    more changes

--- frontend/src/components/MatchCard.tsx
+++ frontend/src/components/MatchCard.tsx
@@ -36,7 +36,7 @@ const MatchCard: React.FC<MatchCardProps> = ({ match }) => {
           <img
             src={match.homeTeamLogo}
             alt={match.homeTeamName}
-            className="object-contain xl:w-[180px] xl:h-[180px] lg:w-[100px] lg:h-[100px] xs:w-[75px] xs:h-[75px] sm:w-[80px] sm:h-[80px] xxs:w-[75px] xxs:h-[75px]"
+            className="object-contain xl:w-[140px] xl:h-[140px] lg:w-[100px] lg:h-[100px] xs:w-[75px] xs:h-[75px] sm:w-[80px] sm:h-[80px] xxs:w-[75px] xxs:h-[75px]"
           />
           <span className="xl:text-2xl lg:text-xl text-white font-[Lato-Bold] mt-3 mb-3 xs:mb-1 xxs:mb-1 sm:mb-2 truncate max-w-[180px]">
             {match.homeTeamName}
@@ -70,7 +70,7 @@ const MatchCard: React.FC<MatchCardProps> = ({ match }) => {
           <img
             src={match.awayTeamLogo}
             alt={match.awayTeamName}
-            className="object-contain xl:w-[180px] xl:h-[180px] lg:w-[100px] lg:h-[100px] xs:w-[75px] xs:h-[75px] sm:w-[80px] sm:h-[80px] xxs:w-[75px] xxs:h-[75px]"
+            className="object-contain xl:w-[140px] xl:h-[140px] lg:w-[100px] lg:h-[100px] xs:w-[75px] xs:h-[75px] sm:w-[80px] sm:h-[80px] xxs:w-[75px] xxs:h-[75px]"
           />
           <span className="xl:text-2xl lg:text-xl text-white font-[Lato-Bold] mt-3 mb-3 sm:mb-1 xs:mb-1 xxs:mb-1 truncate max-w-[180px]">
             {match.awayTeamName}
