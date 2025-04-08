35a96eca63854a484d58e7242b4a0111db0bd20a
portley-0 <oisinportley@protonmail.com>
2025-04-08 14:47:24 +0100

    Fix

--- frontend/src/context/WalletProvider.tsx
+++ frontend/src/context/WalletProvider.tsx
@@ -24,11 +24,11 @@ const connectors = connectorsForWallets(
       groupName: "Recommended",
       wallets: [
         metaMaskWallet,
-        toPrivyWallet({
+        /*toPrivyWallet({
           id: 'cm04asygd041fmry9zmcyn5o5',
           name: 'Abstract (Google)',
           iconUrl: '/images/abstract.png'
-        }),
+        }),*/
         coreWallet,
         rabbyWallet,
         trustWallet,
