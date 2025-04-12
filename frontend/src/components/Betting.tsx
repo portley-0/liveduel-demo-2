2fb628bbbcf4a0252dcd1623b6e8b28db8256d1c
portley-0 <oisinportley@protonmail.com>
2025-04-12 05:55:53 +0100

    Label Edit

--- frontend/src/components/Betting.tsx
+++ frontend/src/components/Betting.tsx
@@ -631,7 +631,7 @@ const Betting: React.FC<{ match: MatchData }> = ({ match }) => {
               {tradeType === "buy" ? (
                 selectedBet === null || betAmount === "" ? (
                   <p>
-                    <strong>You will receive:</strong> 0.00 Outcome Tokens
+                    <strong>You will receive:</strong> 0.00 Tokens
                   </p>
                 ) : (
                   <p>
@@ -653,7 +653,7 @@ const Betting: React.FC<{ match: MatchData }> = ({ match }) => {
                           ${selectedBet.toUpperCase()}
                         </span>{" "}
                         <span className="text-white">
-                          Outcome tokens
+                          Tokens
                         </span>
                       </>
                     )}
@@ -721,7 +721,7 @@ const Betting: React.FC<{ match: MatchData }> = ({ match }) => {
         <div className="bg-greyblue p-6 rounded-lg shadow-lg w-auto max-w-md sm:max-w-xs mx-auto text-center relative z-50">
           <h2 className="text-white text-2xl sm:text-xl font-semibold mb-3">Success</h2>
           <p className="text-gray-300 text-lg sm:text-base">
-            {tradeType === "buy" ? "You purchased" : "You sold"} <span className="text-white font-bold">{modalData.shares}</span> Outcome Tokens
+            {tradeType === "buy" ? "You purchased" : "You sold"} <span className="text-white font-bold">{modalData.shares}</span> Tokens
           </p>
           <p className="text-gray-300 text-lg sm:text-base">
             for <span className="text-white font-bold">${modalData.cost.toFixed(2)}</span> USDC
