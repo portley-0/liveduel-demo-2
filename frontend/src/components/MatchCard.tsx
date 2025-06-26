4e2fa25f0ff672bf576351b1d4893024ccb75f18
portley-0 <oisinportley@protonmail.com>
2025-06-26 21:31:19 +0100

    Large screen aspect ratio

--- frontend/src/components/MatchCard.tsx
+++ frontend/src/components/MatchCard.tsx
@@ -62,16 +62,16 @@ const MatchCard: React.FC<MatchCardProps> = ({ match }) => {
           <img
             src={match.homeTeamLogo}
             alt={match.homeTeamName}
-            className="object-contain 2xl:w-[140px] 2xl:h-[140px] lg:w-[100px] lg:h-[100px] xs:w-[75px] xs:h-[75px] sm:w-[80px] sm:h-[80px] xxs:w-[75px] xxs:h-[75px]"
+            className="object-contain 2xl:w-[280px] 2xl:h-[280px] lg:w-[100px] lg:h-[100px] xs:w-[75px] xs:h-[75px] sm:w-[80px] sm:h-[80px] xxs:w-[75px] xxs:h-[75px]"
           />
-          <span className="2xl:text-2xl lg:text-xl text-white font-[Lato-Bold] mt-3 mb-3 xs:mb-1 xxs:mb-1 sm:mb-2 truncate max-w-[180px]">
+          <span className="2xl:text-4xl lg:text-xl text-white font-[Lato-Bold] mt-3 mb-3 xs:mb-1 xxs:mb-1 sm:mb-2 2xl:mb-5 truncate max-w-[180px]">
             {match.homeTeamName}
           </span>
         </div>
 
         <div className="absolute left-1/2 transform -translate-x-1/2 flex flex-col items-center">
           <span
-            className={`2xl:text-5xl lg:text-4xl sm:text-2xl xs:text-2xl xxs:text-2xl font-bold ${
+            className={`2xl:text-6xl lg:text-4xl sm:text-2xl xs:text-2xl xxs:text-2xl font-bold ${
               match.statusShort &&
               ["1H", "2H", "INT", "BT", "HT", "LIVE", "ET", "P"].includes(match.statusShort)
                 ? "text-redmagenta"
@@ -81,7 +81,7 @@ const MatchCard: React.FC<MatchCardProps> = ({ match }) => {
             {match.homeScore ?? 0}:{match.awayScore ?? 0}
           </span>
           <span
-            className={`2xl:text-xl lg:text-lg sm:text-md font-semibold ${
+            className={`2xl:text-2xl lg:text-lg sm:text-md font-semibold ${
               match.statusShort &&
               ["1H", "2H", "INT", "BT", "HT", "LIVE", "ET", "P"].includes(match.statusShort)
                 ? "text-redmagenta"
@@ -101,15 +101,15 @@ const MatchCard: React.FC<MatchCardProps> = ({ match }) => {
           <img
             src={match.awayTeamLogo}
             alt={match.awayTeamName}
-            className="object-contain 2xl:w-[140px] 2xl:h-[140px] lg:w-[100px] lg:h-[100px] xs:w-[75px] xs:h-[75px] sm:w-[80px] sm:h-[80px] xxs:w-[75px] xxs:h-[75px]"
+            className="object-contain 2xl:w-[280px] 2xl:h-[280px] lg:w-[100px] lg:h-[100px] xs:w-[75px] xs:h-[75px] sm:w-[80px] sm:h-[80px] xxs:w-[75px] xxs:h-[75px]"
           />
-          <span className="2xl:text-2xl lg:text-xl text-white font-[Lato-Bold] mt-3 mb-2 sm:mb-1 xs:mb-1 xxs:mb-1 truncate max-w-[180px]">
+          <span className="2xl:text-4xl lg:text-xl text-white font-[Lato-Bold] mt-3 mb-2 sm:mb-1 xs:mb-1 xxs:mb-1 2xl:mb-5 truncate max-w-[180px]">
             {match.awayTeamName}
           </span>
         </div>
       </div>
 
-      <div className="bg-lightgreyblue 2xl:h-[200px] lg:h-[160px] sm:h-[100px] xs:h-[100px] xxs:h-[100px] min-w-[200px]">
+      <div className="bg-lightgreyblue 2xl:h-[375px] lg:h-[160px] sm:h-[100px] xs:h-[100px] xxs:h-[100px] min-w-[200px]">
         <TradingViewChart
           oddsHistory={match.oddsHistory || { timestamps: [], homeOdds: [], drawOdds: [], awayOdds: [] }}
           matchData={match}
@@ -118,14 +118,14 @@ const MatchCard: React.FC<MatchCardProps> = ({ match }) => {
         />
       </div>
 
-      <div className="flex justify-between items-end mt-2 text-white">
-        <div className="2xl:text-2xl lg:text-xl sm:text-sm xs:text-sm xxs:text-sm font-[Quicksand Bold]">
+      <div className="flex justify-between items-end mt-2 2xl:mt-5 text-white">
+        <div className="2xl:text-3xl lg:text-xl sm:text-sm xs:text-sm xxs:text-sm font-[Quicksand Bold]">
           <span className="block font-semibold">Volume</span>
           <div className="text-white font-semibold">
             ${match.bettingVolume ? (match.bettingVolume / 1_000_000).toFixed(2) : "0.00"}
           </div>
         </div>
-        <div className="flex space-x-4 2xl:text-2xl lg:text-xl sm:text-sm xs:text-sm xxs:text-sm font-[Quicksand Bold]">
+        <div className="flex space-x-4 2xl:text-3xl lg:text-xl sm:text-sm xs:text-sm xxs:text-sm font-[Quicksand Bold]">
           <div className="flex flex-col items-center">
             <span className="text-blue-400 font-semibold">$HOME</span>
             <span className="text-blue-400 font-semibold">{formatOdds(latestHome)}</span>
